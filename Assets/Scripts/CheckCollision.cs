57c40bb417ae4a91f44704ac6ebdcd067c3c8a67
DESKTOP-7KL4ABU\Axel <axeldedconti@gmail.com>
2018-09-10 22:36:04 +0200

    Début prog et création des scenes.

---
+++ Assets/Scripts/CheckCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Object")
+        {
+            GameManager.Singleton.RemoveObject(col.gameObject);
+        }
+        if (col.gameObject.tag == "Death")
+        {
+            GameManager.Singleton.Rebirth();
+        }
+        if (col.gameObject.tag == "Exit")
+        {
+            Debug.Log("Vous êtes sorti");
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Singleton;
+    public int ObjectTotal;
+    public int ObjectIHave;
+    public GameObject Exit;
+    public GameObject Ethan;
+    public GameObject EthanSpawn;
+    public GameObject[] ObjectsToActivate;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Singleton = this;
+        }
+    }
+
+    public void Rebirth()
+    {
+        for (int i = 0; i < ObjectsToActivate.Length; i++)
+        {
+            ObjectsToActivate[i].SetActive(true); 
+        }
+        Ethan.transform.position = EthanSpawn.transform.position;
+        Ethan.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        ObjectIHave = 0;
+        Exit.SetActive(false);
+    }
+
+    public void RemoveObject (GameObject whatToDeactivate)
+    {
+        whatToDeactivate.SetActive(false);
+        ObjectIHave += 1;
+        if (ObjectIHave == ObjectTotal)
+        {
+            Exit.SetActive(true);
+        }
+    }
+}
+        
