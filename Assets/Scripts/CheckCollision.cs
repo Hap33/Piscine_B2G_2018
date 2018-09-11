4749ecc1bd43606ca530a1d130b40d8ccff422c3
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-09-11 13:51:16 +0200

    Ajout Nage

--- Assets/Scripts/CheckCollision.cs
+++ Assets/Scripts/CheckCollision.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CheckCollision : MonoBehaviour {
 
+    private bool Underwater = false;
+    private float MoveY;
     private void OnCollisionEnter(Collision col)
     {
         switch (col.gameObject.tag){
@@ -16,6 +18,9 @@ public class CheckCollision : MonoBehaviour {
             case "Exit":
                 GameManager.Singleton.LoadNextScene();
                 break;
+            /*case "Water":
+                Debug.Log("Dans L'eau"); 
+                break;*/
         }
         /*if (col.gameObject.tag == "Object")
         {   
@@ -30,4 +35,34 @@ public class CheckCollision : MonoBehaviour {
             
         }*/
     }
+    private void FixedUpdate()
+    {
+        MoveY = Input.GetAxis("Vertical");
+        if (MoveY > 0 && Underwater == true)
+        {
+            GetComponent<Rigidbody>().velocity = Camera.main.transform.forward*4f;
+        }
+        if (MoveY == 0 && Underwater == true)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Water")
+        {
+            Underwater = true;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Physics.gravity = new Vector3(0, 0, 0);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Water")
+        {
+            Underwater = false;
+            Physics.gravity = new Vector3(0, -9.8f, 0);
+        }
+    }
+    
 }
