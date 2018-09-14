a85e5aa32aea4feb3b5563370cbf67d01abcb096
Gaspard BROUSSAUD <gaspard.broussaud1999@gmail.com>
2018-09-14 13:41:31 +0200

    Change script Menu Accueil

--- Assets/Scripts/UI_Selection_Des_Niveaux.cs
+++ Assets/Scripts/UI_Selection_Des_Niveaux.cs
@@ -4,65 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UI_Selection_Des_Niveaux : MonoBehaviour {
-
-    /*public GameObject UI_MenuSelection;
-    public GameObject GameManagerObject;
-    public GameObject EventSystemSelec;
-    public GameObject EventSystemAccueil;
-    public GameObject UI_MenuAccueil;*/
+    
     public AudioSource Source;
     public AudioClip sonBouton;
 
-    public int[] tabMenu;
-
-    public void OnClickButton01()
+    protected void HandlePointerExitAndEnter(EventSystems.PointerEventData currentPointerData, GameObject newEnterTarget)
     {
-        Source.PlayOneShot(sonBouton);
-        SceneManager.LoadScene("Level_01");
+
     }
 
-    public void OnClickButton02()
+    public void OnClickButtonChangeScene(string sceneLoadName)
     {
         Source.PlayOneShot(sonBouton);
-        SceneManager.LoadScene("Level_02");
+        SceneManager.LoadScene(sceneLoadName);
     }
 
-    public void OnClickButton03()
+    public void OnClickButtonChangeObjects()
     {
         Source.PlayOneShot(sonBouton);
-        SceneManager.LoadScene("Level_03");
-    }
-
-    public void OnClickButton04()
-    {
-        Source.PlayOneShot(sonBouton);
-        SceneManager.LoadScene("Level_04");
-    }
-
-    public void OnClickButton05()
-    {
-        Source.PlayOneShot(sonBouton);
-        SceneManager.LoadScene("Level_05");
-    }
-
-    public void OnClickButtonAccueil()
-    {
-        Source.PlayOneShot(sonBouton);
-        /*UI_MenuSelection.SetActive(false);
-        GameManagerObject.SetActive(false);
-        EventSystemSelec.SetActive(false);
-        EventSystemAccueil.SetActive(true);
-        UI_MenuAccueil.SetActive(true);*/
-    }
-
-    public void OnClickJouer()
-    {
-        Source.PlayOneShot(sonBouton);
-        /*gameObject.SetActive(false);
-        EventSystemAccueil.SetActive(false);
-        EventSystemSelec.SetActive(true);
-        GameManagerObject.SetActive(true);
-        UI_MenuSelection.SetActive(true);*/
     }
 
     public void OnClickQuitter()
