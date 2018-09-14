9959c50885d892fbdff5ac3ebd74a4e085d7dcfd
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-09-14 13:19:20 +0200

    Souris invisible

---
+++ Assets/Scripts/NoMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoMouse : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Cursor.visible = false;
+    }
+}
