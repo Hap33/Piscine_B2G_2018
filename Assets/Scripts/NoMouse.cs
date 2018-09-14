9900e70d4a6cec96f467df1907bd9b59e8d2c6cf
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-09-14 13:50:02 +0200

    remodif souris

--- Assets/Scripts/NoMouse.cs
+++ Assets/Scripts/NoMouse.cs
@@ -7,6 +7,10 @@ public class NoMouse : MonoBehaviour {
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+    private void FixedUpdate()
+    {
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
